a2c7e21a185a5b1d23cddca41328cc7df4c0746d
pavel <kalashnikovisme@gmail.com>
2015-02-21 01:10:41 +0300

    add user model

---
+++ db/migrate/20150220220847_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.text :email
+      t.text :password_digest
+
+      t.timestamps null: false
+    end
+  end
+end
