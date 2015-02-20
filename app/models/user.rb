ea7e958d9e1648a4cf6e61974057344e5a7849a1
pavel <kalashnikovisme@gmail.com>
2015-02-21 01:26:15 +0300

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :email, presence: true
+
+  extend Enumerize
+  enumerize :role, in: [ :user, :admin ], default: :user
 end
