79a60fb28b84fb463d3b4ec286a7140d52bb8c57
pavel <kalashnikovisme@gmail.com>
2015-03-01 20:21:19 +0300

    add menu_item to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def menu_item(name = nil, path = '#', *args, &block)
+    path = name || path if block_given?
+    options = args.extract_options!
+    content_tag :li, class: is_active?(path, options) do
+      name, path = path, options if block_given?
+      link_to name, path, options, &block
+    end
+  end
+
+  def is_active?(path, options = {})
+    'active' if uri_state(path, options).in? [:active, :chosen]
+  end
+
+  def uri_state(uri, options = {})
+    root_url = request.host_with_port + '/'
+    root = uri == '/' || uri == root_url
+    request_uri = if uri.start_with? root_url
+      request.url
+    else
+      request.path
+    end
+    if !options[:method].nil? || !options["data-method"].nil?
+      :inactive
+    elsif uri == request_uri || (options[:root] && (request_uri == '/') || (request_uri == root_url))
+      :active
+    else
+      if request_uri.start_with?(uri) and not(root)
+        :chosen
+      else
+        :inactive
+      end
+    end
+  end
 end
