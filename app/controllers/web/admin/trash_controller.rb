8ce3f6a7f869caf0a289bac8ea47cbc1b5f3c95c
pavel <kalashnikovisme@gmail.com>
2015-03-01 23:33:32 +0300

    unworking patch and delete links

--- app/controllers/web/admin/trash_controller.rb
+++ app/controllers/web/admin/trash_controller.rb
@@ -1,19 +1,19 @@
 class Web::Admin::TrashController < Web::Admin::ApplicationController
   def index
     @type = params[:type].capitalize
-    @items = Object.const_get("#{@type}Decorator").decorate_collection Object.const_get(@type).removed
+    @items = "#{@type}Decorator".constantize.decorate_collection @type.constantize.removed
   end
 
   def restore
     type = params[:type]
-    item = Object.const_get(type.capitalize).find params[:id]
+    item = type.capitalize.constantize.find params[:id]
     item.restore
     redirect_to "/admin/trash/index/#{type}"
   end
 
   def destroy
     type = params[:type]
-    item = Object.const_get(type.capitalize).find params[:id]
+    item = type.capitalize.constantize.find params[:id]
     item.destroy
     redirect_to "/admin/trash/index/#{type}"
   end
