2a99e0f4357e3a104a4726bf97d8c30d8e57394d
AlisVero <soxat73rus@gmail.com>
2015-03-03 01:08:17 +0300

    create News model

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,15 @@
 class News < ActiveRecord::Base
+  mount_uploader :photo, PhotoUploader
+  validates :title,         presence: true
+  validates :body,          presence: true
+  validates :published_at,  presence: true
+  validates :photo,         presence: true
+  validates :author_id,     presence: true
+
+  include NewsRepository
+
+  def is_published?
+    published_at <= DateTime.now
+  end
+
 end
