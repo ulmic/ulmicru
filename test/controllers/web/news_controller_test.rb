bd99020f26f7c14ba0351ae580134140b4261dd3
AlisVero <soxat73rus@gmail.com>
2015-03-02 22:11:32 +0300

    generate new news_controller

---
+++ test/controllers/web/news_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Web::NewsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/decorators/web/news_decorator_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Web::NewsDecoratorTest < Draper::TestCase
+end
