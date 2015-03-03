8d81255a6633c9c0c5365133fa0d23f2a70cc9d9
AlisVero <soxat73rus@gmail.com>
2015-03-03 03:51:10 +0300

    Index show all news

--- app/controllers/web/news_controller.rb
+++ app/controllers/web/news_controller.rb
@@ -1,12 +1,12 @@
 class Web::NewsController < Web::ApplicationController
   def index
-    
+    @news = NewsDecorator.decorate_collection News.published.order('created_at DESC')
   end
 
   def show
     @news = News.find(params[:id]).decorate
     if !@news.is_published?
-      redirect_to not_found_errors_path
+#FIXME there 404 error path
     end
   end
 end
