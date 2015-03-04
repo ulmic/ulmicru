cb8c1e36109f455d6b1740918a8354ff718a23e9
AlisVero <soxat73rus@gmail.com>
2015-03-04 23:49:51 +0300

    add admin controller for news

---
+++ app/controllers/web/admin/news_controller.rb
@@ -0,0 +1,33 @@
+class Web::NewsController < Web::ApplicationController
+  def index
+    @published_news = Web::NewsDecorator.decorate_collection News.published.order('created_at DESC')
+    @unpublished_news = Web::NewsDecorator.decorate_collection News.published.order('created_at DESC')
+  end
+
+  def show
+    @news = News.find(params[:id]).decorate #FIXME I'm don't know how to include decorate there into code
+    if !@news.is_published?
+      #FIXME there 404 error path
+    end
+  end
+
+  def create
+
+  end
+
+  def new
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
