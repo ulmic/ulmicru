13dfa3761289c1ba5310e2eeb7018715e35c3bb8
AlisVero <soxat73rus@gmail.com>
2015-03-04 23:53:04 +0300

    Update namespace for NewsDecorator

--- app/controllers/web/news_controller.rb
+++ app/controllers/web/news_controller.rb
@@ -1,12 +1,12 @@
 class Web::NewsController < Web::ApplicationController
   def index
-    @news = NewsDecorator.decorate_collection News.published.order('created_at DESC')
+    @news = Web::NewsDecorator.decorate_collection News.published.order('created_at DESC')
   end
 
   def show
-    @news = News.find(params[:id]).decorate
+    @news = News.find(params[:id]).decorate #FIXME I'm don't know how to include decorate there into code
     if !@news.is_published?
-#FIXME there 404 error path
+      #FIXME there 404 error path
     end
   end
 end
