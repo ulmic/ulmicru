8cf527e8ca76636ce8309c3f99e55527892213f0
AlisVero <soxat73rus@gmail.com>
2015-03-05 10:07:58 +0300

    Created News admin controller

--- app/controllers/web/admin/news_controller.rb
+++ app/controllers/web/admin/news_controller.rb
@@ -12,22 +12,39 @@ class Web::Admin::NewsController < Web::Admin::ApplicationController
   end
 
   def create
-
+    @news = News.new
+    @news_form = NewsForm.new @news
+    @news_form.submit params[:user]
+    if @news_form.save
+      redirect_to admin_news_path
+    else 
+      redirect_to action: :new
+    end
   end
-
   def new
-    
+    @news = News.new
+    @news_form = NewsForm.new @news
   end
 
   def edit
-
+    @news = News.find params[:id]
+    @news_form = NewsForm.new @news
   end
 
   def update
-
+    @news = News.find params[:id]
+    @news_form = NewsForm.new(@news)
+    @news_form.submit params[:user]
+    if @news_form.save
+      redirect_to admin_news_path
+    else 
+      redirect_to action: :edit
+    end
   end
 
   def destroy
-
+    @news = News.find params[:id]
+    @news.remove
+    redirect_to admin_news_path
   end
 end
