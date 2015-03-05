8410224f476b36ea73151d6cf75db64bab09fd33
AlisVero <soxat73rus@gmail.com>
2015-03-05 13:41:30 +0300

    fix wrong form for create news

--- test/factories/news.rb
+++ test/factories/news.rb
@@ -4,6 +4,6 @@ FactoryGirl.define do
     body         { generate :string } 
     published_at { DateTime.now }
     photo        { generate :file }
-    author_id    { generate :number }
+    author_id    { "1" }
   end
 end
