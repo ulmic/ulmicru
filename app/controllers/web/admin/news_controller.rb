52b8c8eff3f20059d2e57bbaa5c06335f8d5324e
AlisVero <soxat73rus@gmail.com>
2015-03-05 12:17:48 +0300

    Update namespaces

--- app/controllers/web/admin/news_controller.rb
+++ app/controllers/web/admin/news_controller.rb
@@ -1,11 +1,11 @@
 class Web::Admin::NewsController < Web::Admin::ApplicationController
   def index
-    @published_news = Web::NewsDecorator.decorate_collection News.published.order('created_at DESC')
-    @unpublished_news = Web::NewsDecorator.decorate_collection News.published.order('created_at DESC')
+    @published_news = Web::NewsDecorator.decorate_collection News.published.order('created_at DESC') #FIXME using another decorator in that file
+    @unpublished_news = Web::NewsDecorator.decorate_collection News.unpublished.order('created_at DESC')
   end
 
   def show
-    @news = News.find(params[:id]).decorate #FIXME I'm don't know how to include decorate there into code
+    @news = Web::NewsDecorator.decorate News.find params[:id]#FIXME I'm don't know how to include decorate there into code
     if !@news.is_published?
       #FIXME there 404 error path
     end
