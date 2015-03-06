3f28fe89fd7708e2f42dc5a60f48da2e4bb09a84
AlisVero <soxat73rus@gmail.com>
2015-03-07 00:49:27 +0400

    update gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150303003907) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authentications", force: :cascade do |t|
     t.text     "provider"
     t.text     "uid"
