70b86d456aaeeb09e6320d4a437f507ea1a07738
AlisVero <soxat73rus@gmail.com>
2015-03-07 16:04:55 +0300

    update db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303003907) do
+ActiveRecord::Schema.define(version: 20150302192318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20150303003907) do
     t.string   "title"
     t.text     "body"
     t.datetime "published_at"
+    t.text     "photo"
+    t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.text     "photo"
-    t.integer  "author_id"
   end
 
   create_table "users", force: :cascade do |t|
