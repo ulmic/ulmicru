9664836f3c317216b256a49050830b5149b301f4
AlisVero <soxat73rus@gmail.com>
2015-03-08 10:52:58 +0300

    Add state_machine to News

--- app/models/news.rb
+++ app/models/news.rb
@@ -16,4 +16,25 @@ class News < ActiveRecord::Base
   scope :published, -> { where 'published_at <= ?', DateTime.now}
   scope :unpublished, -> { where 'published_at > ?', DateTime.now}
 
+  scope :removed, -> { where state: :removed }
+  
+  state_machine :state, initial: :unviewed do
+    state :unviewed
+    state :confirmed
+    state :declined
+    state :removed
+
+    event :confirm do
+      transition all => :confirmed
+    end
+    event :decline do
+      transition all => :declined
+    end
+    event :remove do
+      transition all => :removed
+    end
+    event :restore do
+      transition :removed => :unviewed
+    end
+  end
 end
