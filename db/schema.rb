0b7d124d74ebd7eaa5f1303b505d68229b86523d
AlisVero <soxat73rus@gmail.com>
2015-03-10 01:38:58 +0300

    rake db:migrate :)

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308075000) do
+ActiveRecord::Schema.define(version: 20150309113504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.string   "view"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "attribute_accesses", force: :cascade do |t|
     t.integer  "member_id"
@@ -54,10 +63,10 @@ ActiveRecord::Schema.define(version: 20150308075000) do
     t.text     "body"
     t.datetime "published_at"
     t.text     "photo"
-    t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.text     "state"
+    t.integer  "user_id"
   end
 
   create_table "questionaries", force: :cascade do |t|
