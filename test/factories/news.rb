cef8111006c92ebe976ead279a800f93305d003f
AlisVero <soxat73rus@gmail.com>
2015-03-12 22:09:09 +0300

    change :file to :string for fastre testing

--- test/factories/news.rb
+++ test/factories/news.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     title        { generate :string }
     body         { generate :string }
     published_at { DateTime.now }
-    photo        { generate :file }
-    lead         {generate :string}
+    photo        { generate :string }
+    lead         { generate :string }
     user_id      { generate :integer }
   end
 end
