ed73f4329ffe127bd9a19b4380756a26d95613db
pavel <kalashnikovisme@gmail.com>
2015-03-13 16:08:22 +0300

    deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+server 'ulmic.ru', user: 'ulmic', roles: %w{web app db}
+
+set :rails_env, :production
+set :branch, "master"
---
+++ config/deploy/staging.rb
@@ -0,0 +1,17 @@
+server 'staging.ulmic.ru', user: 'ulmic', roles: %w{web app db}
+
+set :application, 'ulmic_stg'
+set :deploy_to, '/srv/ulmic_stg'
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+set :rails_env, :staging
+set :branch, "develop"
+
+namespace :deploy do
+  task :restart do
+    puts "DONT DEPLOY TO STAGING!!!! DONT!!!"
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+
+  after :publishing, :restart
+end
