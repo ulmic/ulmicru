8674e5dba72cdeaf89577e89f3b7b4699b3ae78e
pavel <kalashnikovisme@gmail.com>
2015-03-15 11:59:39 +0300

    remove basic_aut

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  if Rails.env.staging?
-    before_filter :required_basic_auth!
-  end
+  #if Rails.env.staging?
+
+  #before_filter :required_basic_auth!
+
+#end
 end
