7a40cda22a250dd1b68010e9c2a209d7ae5d93d8
pavel <kalashnikovisme@gmail.com>
2015-03-15 16:40:17 +0300

    fix test

--- test/controllers/web/members_controller_test.rb
+++ test/controllers/web/members_controller_test.rb
@@ -19,7 +19,7 @@ class Web::MembersControllerTest < ActionController::TestCase
 
   test 'should create member' do
     attributes = attributes_for :member
-    post :create, user: attributes
+    post :create, member: attributes
     assert_response :redirect, @response.body
     assert_redirected_to account_path
     assert_equal attributes[:patronymic], Member.last.patronymic
