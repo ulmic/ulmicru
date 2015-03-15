92b962a1066493498dde2f25660544ae6fdaa113
pavel <kalashnikovisme@gmail.com>
2015-03-15 16:19:50 +0300

    return basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #if Rails.env.staging?
-
-  #before_filter :required_basic_auth!
-
-#end
+  if Rails.env.staging?
+    before_filter :required_basic_auth!
+  end
 end
