7cb32850e6a3603e84894ec8b99d79ec7b7d3524
AlisVero <soxat73rus@gmail.com>
2015-03-16 18:48:37 +0300

    Add validator for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,7 @@ class Category < ActiveRecord::Base
   belongs_to :parent,     class_name: 'Category'
   has_many   :articles
 
+  validates_with CategoryValidator
   validates :name,      presence: true
   validates :parent_id, presence: false
   validates :is_last,   presence: false
