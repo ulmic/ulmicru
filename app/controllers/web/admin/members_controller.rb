0320383a444cacb93d5b401956e276d329509ebc
pavel <kalashnikovisme@gmail.com>
2015-03-18 17:56:11 +0300

    fix member form

--- app/controllers/web/admin/members_controller.rb
+++ app/controllers/web/admin/members_controller.rb
@@ -14,9 +14,8 @@ class Web::Admin::MembersController < Web::Admin::ApplicationController
   end
 
   def create
-    @member = Member.new
-    @member_form = MemberForm.new(@member)
-    @member_form.submit(params[:member])
+    @member_form = MemberForm.new_with_model
+    @member_form.submit params[:member]
     if @member_form.save
       redirect_to admin_members_path
     else
