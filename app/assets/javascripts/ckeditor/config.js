ea545b9e1ca063291f0c3a6c0b3956841e8444e2
pavel <kalashnikovisme@gmail.com>
2015-03-20 01:34:13 +0300

    add youtube ckeditor plugin

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,7 @@
+CKEDITOR.editorConfig = function (config) {
+  config.toolbar_mini = [
+    ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
+  ];
+  config.toolbar = "simple";
+  config.extraPlugins = 'youtube';
+}
