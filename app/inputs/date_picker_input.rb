aaccf268d4d3c242020316f35d48af3217a8b7be
pavel <kalashnikovisme@gmail.com>
2015-03-25 19:10:09 +0300

    fix date_time_input

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,22 +1,24 @@
 class DatePickerInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options)
     value = object.send(attribute_name) if object.respond_to? attribute_name
-    display_pattern = I18n.t('datepicker.dformat', :default => '%d/%m/%Y')
-    value = value.to_datetime if value.is_a? String
-    input_html_options[:value] ||= I18n.localize(value, :format => display_pattern) if value.present?
+    display_pattern = I18n.t('datepicker.dformat', default: '%d/%m/%Y')
+    unless value == 'for_now'
+      value = value.to_datetime if value.is_a? String
+      input_html_options[:value] ||= I18n.localize(value, format: display_pattern) if value.present?
+    end
 
     input_html_options[:type] = 'text'
-    picker_pattern = I18n.t('datepicker.pformat', :default => 'DD/MM/YYYY')
-    dayViewHeaderFormat = I18n.t('dayViewHeaderFormat', :default => 'MMMM YYYY')
+    picker_pattern = I18n.t('datepicker.pformat', default: 'DD/MM/YYYY')
+    dayViewHeaderFormat = I18n.t('dayViewHeaderFormat', default: 'MMMM YYYY')
     date_options = {
-        locale: I18n.locale.to_s,
-        dayViewHeaderFormat: dayViewHeaderFormat,
-        format: picker_pattern
+      locale: I18n.locale.to_s,
+      dayViewHeaderFormat: dayViewHeaderFormat,
+      format: picker_pattern
     }
     input_html_options[:data] ||= {}
     input_html_options[:data].merge!({date_options: date_options })
 
-    template.content_tag :div, class: 'input-group date datepicker' do
+    template.content_tag :div, class: "input-group date datepicker #{ value == 'for_now' ? :disabled : '' }" do
       input = super(wrapper_options) # leave StringInput do the real rendering
       input += template.content_tag :span, class: 'input-group-btn' do
         template.content_tag :button, class: 'btn btn-default', type: 'button' do
