8130a242dbfe1014b63b0a4dc59599129e9bc3e9
pavel <kalashnikovisme@gmail.com>
2015-03-28 02:27:47 +0300

    fix tests

--- test/factories/events.rb
+++ test/factories/events.rb
@@ -2,11 +2,10 @@ FactoryGirl.define do
   factory :event do
     title "MyString"
     description "MyString"
-    main_photo "MyString"
+    main_photo        { generate :file }
     creator_id 1
     begin_date "2015-03-06 14:36:16"
     end_date "2015-03-06 14:36:16"
     event_type "big"
   end
-
 end
