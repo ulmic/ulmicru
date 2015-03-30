07bc897946051c751c5a9572ca1f2a378111cc42
pavel <kalashnikovisme@gmail.com>
2015-03-31 00:44:10 +0300

    korporative category

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -5,5 +5,8 @@ class Web::ApplicationController < ApplicationController
 
   def load_categories_tree
     @first_category = Category.find_by_name 'Кто мы такие'
+    if signed_in?
+      @korporative_category = Category.find_by_name 'Корпоративные проекты'
+    end
   end
 end
