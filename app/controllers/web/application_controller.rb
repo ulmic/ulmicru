95aca48d51a48d081f42cd83c811d09f2b5b983d
pavel <kalashnikovisme@gmail.com>
2015-04-03 02:46:03 +0300

    add contacts to navbar

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -3,6 +3,8 @@ class Web::ApplicationController < ApplicationController
 
   def load_categories_tree
     @first_category = Category.find_by_name 'Кто мы такие'
+    contact_category = Category.find_by_name 'Контакты'
+    @contact_article = contact_category.articles.first if contact_category
     if signed_in?
       @korporative_category = Category.find_by_name 'Корпоративные проекты'
     end
