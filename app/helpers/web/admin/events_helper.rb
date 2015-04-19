bf0efae8d7df84a2d290235a2902316c93d64583
pavel <kalashnikovisme@gmail.com>
2015-04-20 01:09:49 +0300

    fix tests

--- app/helpers/web/admin/events_helper.rb
+++ app/helpers/web/admin/events_helper.rb
@@ -9,16 +9,20 @@ module Web::Admin::EventsHelper
 
   def teams_and_members_hash(teams, members)
     hash = {}
-    teams_hash = {}
-    teams.each do |team|
-      teams_hash[team.full_title] = team.id
+    if teams
+      teams_hash = {}
+      teams.each do |team|
+        teams_hash[team.full_title] = team.id
+      end
+      hash[t('activerecord.models.team')] = teams_hash
     end
-    members_hash = {}
-    members.each do |member|
-      members_hash[member.select_presentation] = member.id
+    if members
+      members_hash = {}
+      members.each do |member|
+        members_hash[member.select_presentation] = member.id
+      end
+      hash[t('activerecord.models.member')] = members_hash
     end
-    hash[t('activerecord.models.team')] = teams_hash
-    hash[t('activerecord.models.member')] = members_hash
     hash
   end
 end
