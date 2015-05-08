6f426287ce21ba7b5de499c9b8a0004829e20af3
pavel <kalashnikovisme@gmail.com>
2015-05-08 12:04:08 +0300

    production deploy

---
+++ config/unicorn/production.rb
@@ -0,0 +1,29 @@
+app_path = '/srv/ulmicru'
+working_directory "#{app_path}/current"
+
+worker_processes 1
+
+listen app_path + "/shared/.sock", backlog: 64
+
+timeout 15
+# preload_app true
+
+stderr_path "#{app_path}/current/log/unicorn.stderr.log"
+stdout_path "#{app_path}/current/log/unicorn.stdout.log"
+
+# FIXME
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{working_directory}/Gemfile"
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
