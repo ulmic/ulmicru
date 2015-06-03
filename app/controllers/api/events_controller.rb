1f3ea3f0dc6a2b64d0728d0ccaaab175e8572b19
pavel <kalashnikovisme@gmail.com>
2015-06-03 10:47:21 +0300

    fix events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,6 +1,6 @@
 class Api::EventsController < Api::ApplicationController
   def index
-    events = EventDecorator.decorate Event.active.drop(params[:offset].to_i).first(params[:count].to_i)
+    events = EventDecorator.decorate Event.active.order('end_date ASC').drop(params[:offset].to_i).first(params[:count].to_i)
     hash = []
     events.each { |event| event = event.decorate; hash << { id: event.id, photo: event.main_photo, title: event.title, text: event.lead, date: event.full_date_duration, category: event.activity_line.title } }
     last_events = true if events.count < 8
