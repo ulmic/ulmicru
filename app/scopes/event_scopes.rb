c6008f702e060ee541076688825e8eef7cd0d23f
pavel <kalashnikovisme@gmail.com>
2015-06-09 17:57:08 +0300

    fix event image on main page

--- app/scopes/event_scopes.rb
+++ app/scopes/event_scopes.rb
@@ -9,7 +9,7 @@ module EventScopes
     scope :unviewed, -> { where(state: :unviewed)  }
     scope :future, -> { where('begin_date >= ?', DateTime.now).where.not(state: :removed) }
     scope :current, -> { where('begin_date < ? AND end_date > ?', DateTime.now, DateTime.now).where.not(state: :removed) }
-    scope :active, -> { where('end_date > ?', DateTime.now).where.not(state: :removed)  }
+    scope :active, -> { where('end_date > ?', DateTime.now).where(state: :confirmed)  }
     scope :past, -> { where('end_date <= ?', DateTime.now).where.not(state: :removed) }
     scope :near_future, -> { where('begin_date <= ?', DateTime.now + 3.month).order('end_date DESC') }
   end
