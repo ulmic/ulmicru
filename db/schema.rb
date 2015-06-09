9e7fc2579a067db47f22d743dadb5d5653b6d6c7
pavel <kalashnikovisme@gmail.com>
2015-06-09 16:54:14 +0300

    add school to members

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609130615) do
+ActiveRecord::Schema.define(version: 20150609135136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -274,6 +274,7 @@ ActiveRecord::Schema.define(version: 20150609130615) do
     t.datetime "updated_at",      null: false
     t.datetime "join_date"
     t.text     "token"
+    t.text     "school"
   end
 
 end
