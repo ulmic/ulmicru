b2037a7d2931ca2d397e14bb21415c8c42d35d38
pavel <kalashnikovisme@gmail.com>
2015-06-09 16:49:17 +0300

    add primaries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527003607) do
+ActiveRecord::Schema.define(version: 20150609130615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,8 @@ ActiveRecord::Schema.define(version: 20150527003607) do
     t.text     "state"
     t.text     "municipality"
     t.text     "type"
+    t.text     "school"
+    t.integer  "team_id"
   end
 
   create_table "teams_users", id: false, force: :cascade do |t|
--- lib/schools.rb
+++ lib/schools.rb
@@ -1,5 +1,15 @@
 module Schools
   def self.list
-    YAML.load_file("#{Rails.root}/lib/yaml/schools.yml")['schools']
+    schools = YAML.load_file("#{Rails.root}/lib/yaml/schools.yml")['schools']
+    list = []
+    schools.each do |municipality|
+      list += municipality[1]
+    end
+    list
+  end
+
+  def self.list_hash
+    hash = YAML.load_file("#{Rails.root}/lib/yaml/schools.yml")['schools']
+    hash.to_a
   end
 end
