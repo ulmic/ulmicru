00cd425a0de7e4f7bb4c94eb548e9fe6efb81648
pavel <kalashnikovisme@gmail.com>
2015-06-13 20:39:06 +0300

    remove order in events API

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,6 +1,6 @@
 class Api::EventsController < Api::ApplicationController
   def index
-    events = EventDecorator.decorate Event.active.near_future.order('end_date DESC').drop(params[:offset].to_i).first(params[:count].to_i)
+    events = EventDecorator.decorate Event.active.near_future.drop(params[:offset].to_i).first(params[:count].to_i)
     hash = []
     events.each { |event| event = event.decorate; hash << { id: event.id, photo: event.main_photo.event_main_page_version, title: event.title, text: event.lead, date: event.full_date_duration, category: event.activity_line.title } }
     last_events = true if events.count < 8
