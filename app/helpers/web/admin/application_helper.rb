40f3b6115c0252108521c1eb189c02137ce80512
pavel <kalashnikovisme@gmail.com>
2015-06-14 12:36:56 +0300

    #126 count of records on users

--- app/helpers/web/admin/application_helper.rb
+++ app/helpers/web/admin/application_helper.rb
@@ -7,4 +7,8 @@ module Web::Admin::ApplicationHelper
       :success if item.unviewed?
     end
   end
+
+  def tab_title(model_class, tab, count)
+    "#{t("state_machines.#{model_class.name.downcase}.state.states.#{tab}").pluralize(:ru)} / #{count}"
+  end
 end
