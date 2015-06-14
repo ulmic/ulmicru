fdf4be30849950ca514fa632b8483c6100557c66
pavel <kalashnikovisme@gmail.com>
2015-06-14 12:03:26 +0300

    fix notification count

--- app/helpers/web/admin/application_helper.rb
+++ app/helpers/web/admin/application_helper.rb
@@ -2,10 +2,6 @@ module Web::Admin::ApplicationHelper
   include Localities
   include Places
 
-  def notification_count
-    Member.unviewed.count + Questionary.unviewed.count + News.unviewed.count + Event.unviewed.count
-  end
-
   def state_color(item)
     if item.methods.include? :unviewed?
       :success if item.unviewed?
