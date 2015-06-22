507d65411294ede95f68360d948fb811910e1798
pavel <kalashnikovisme@gmail.com>
2015-06-22 20:15:08 +0300

    #115 some fixes for articles

--- app/scopes/article_scopes.rb
+++ app/scopes/article_scopes.rb
@@ -4,6 +4,9 @@ module ArticleScopes
 
   included do
     scope :removed, -> { where state: :removed }
-    scope :presented, -> { where.not(state: :removed).order('id ASC')}
+    scope :presented, -> { where.not(state: :removed).order('id DESC')}
+    scope :confirmed, -> { where(state: :confirmed).order('id DESC') }
+    scope :inactive, -> { where(state: :inactive).order('id DESC') }
+    scope :unviewed, -> { where(state: :unviewed).order('id DESC') }
   end
 end
