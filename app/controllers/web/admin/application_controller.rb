606ed34f453c6cbd3e530c400453a40397ae337b
pavel <kalashnikovisme@gmail.com>
2015-06-23 12:11:42 +0300

    add notification count for admin

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -1,6 +1,5 @@
 class Web::Admin::ApplicationController < Web::ApplicationController
   before_filter :authenticate_admin!
-  before_filter :notification_count
   layout 'web/admin/application'
 
   protected
@@ -11,13 +10,5 @@ class Web::Admin::ApplicationController < Web::ApplicationController
 
   def choose_teams
     @teams = Team.active.decorate
-  end
-
-  def notification_count
-    collections = [ :member, :questionary, :news, :event, :user, :feedback ]
-    @notification_count = 0
-    collections.each do |collection_type|
-      @notification_count += collection_type.to_s.capitalize.constantize.unviewed.count
-    end
   end
 end
