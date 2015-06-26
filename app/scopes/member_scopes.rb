2696975da82016a50ebc4edf2b7ba892d1f0f0d3
pavel <kalashnikovisme@gmail.com>
2015-06-26 09:47:50 +0300

    update member scopes

--- app/scopes/member_scopes.rb
+++ app/scopes/member_scopes.rb
@@ -3,11 +3,11 @@ module MemberScopes
   include Concerns::StateMachine
 
   included do
-    scope :presented, -> { where('state != \'removed\' AND state != \'not_member\'').order('ticket ASC') }
-    scope :confirmed, -> { where(state: :confirmed).order('ticket DESC') }
-    scope :declined, -> { where(state: :declined).order('ticket DESC') }
-    scope :removed, -> { where(state: :removed).order('ticket DESC') }
-    scope :unviewed, -> { where(state: :unviewed, type: 'Member').order('id ASC') }
+    scope :presented, -> { where('member_state != \'removed\' AND member_state != \'not_member\'').order('ticket ASC') }
+    scope :confirmed, -> { where(member_state: :confirmed).not(ticket: nil).order('ticket DESC') }
+    scope :declined, -> { where(member_state: :declined).order('ticket DESC') }
+    scope :removed, -> { where(member_state: :removed).order('ticket DESC') }
+    scope :unviewed, -> { where(member_state: :unviewed, type: 'Member').order('id ASC') }
     scope :unavailable, -> { where(state: :unavailable).order('ticket ASC') }
   end
 end
