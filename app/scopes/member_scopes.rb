a7b3d8f754e29cb2ce7dfd1980c409b288abb2f4
pavel <kalashnikovisme@gmail.com>
2015-06-26 10:06:26 +0300

    fix member scopes

--- app/scopes/member_scopes.rb
+++ app/scopes/member_scopes.rb
@@ -4,10 +4,10 @@ module MemberScopes
 
   included do
     scope :presented, -> { where('member_state != \'removed\' AND member_state != \'not_member\'').order('ticket ASC') }
-    scope :confirmed, -> { where(member_state: :confirmed).where.not(ticket: nil).order('ticket DESC') }
-    scope :declined, -> { where(member_state: :declined).order('ticket DESC') }
+    scope :confirmed, -> { where(member_state: :confirmed).where.not(ticket: nil, state: :unavailable).order('ticket DESC') }
+    scope :declined, -> { where(member_state: :declined).where.not(state: :unavailable).order('ticket DESC') }
     scope :removed, -> { where(member_state: :removed).order('ticket DESC') }
-    scope :unviewed, -> { where(member_state: :unviewed, type: 'Member').order('id ASC') }
+    scope :unviewed, -> { where(member_state: :unviewed, type: 'Member').where.not(state: :unavailable).order('id ASC') }
     scope :unavailable, -> { where(state: :unavailable).order('ticket ASC') }
   end
 end
