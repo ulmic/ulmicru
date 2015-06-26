6cae0bffb1bc9d3053c6cc7d122f78fb870c6e78
pavel <kalashnikovisme@gmail.com>
2015-06-26 08:05:24 +0300

    edit order on member scope

--- app/scopes/member_scopes.rb
+++ app/scopes/member_scopes.rb
@@ -4,10 +4,10 @@ module MemberScopes
 
   included do
     scope :presented, -> { where('state != \'removed\' AND state != \'not_member\'').order('ticket ASC') }
-    scope :confirmed, -> { where(state: :confirmed).order('id ASC') }
-    scope :declined, -> { where(state: :declined).order('id ASC') }
-    scope :removed, -> { where(state: :removed).order('id ASC') }
-    scope :unviewed, -> { where(state: :unviewed, type: 'Member').order('ticket DESC') }
-    scope :unavailable, -> { where(state: :unavailable).order('id ASC') }
+    scope :confirmed, -> { where(state: :confirmed).order('ticket DESC') }
+    scope :declined, -> { where(state: :declined).order('ticket DESC') }
+    scope :removed, -> { where(state: :removed).order('ticket DESC') }
+    scope :unviewed, -> { where(state: :unviewed, type: 'Member').order('id ASC') }
+    scope :unavailable, -> { where(state: :unavailable).order('ticket ASC') }
   end
 end
