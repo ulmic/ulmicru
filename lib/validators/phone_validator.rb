d3a5f3b470fb4623e851b80fcd418c601c67bb93
pavel <kalashnikovisme@gmail.com>
2015-07-07 01:09:50 +0300

    #161 fix phone validator

--- lib/validators/phone_validator.rb
+++ lib/validators/phone_validator.rb
@@ -1,7 +1,6 @@
 class PhoneValidator < ActiveModel::EachValidator
-
   def validate_each(record, attribute, value)
-    unless value =~ /^\+\d{,3}[\s\(-]*\d{3}[\s\)-]*[\d\s-]*$/
+    unless value =~ /^\+|8\d{,3}[\s\(-]*\d{3}[\s\)-]*[\d\s-]*$/
       record.errors.add(attribute, :phone, options.merge(value: value))
     end
   end
