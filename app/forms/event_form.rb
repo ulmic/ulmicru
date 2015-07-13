c32d56af5678aa9922a639e68dd56cf83302bd26
pavel <kalashnikovisme@gmail.com>
2015-07-13 04:01:18 +0300

    working actionform remove button

--- app/forms/event_form.rb
+++ app/forms/event_form.rb
@@ -4,6 +4,6 @@ class EventForm < ApplicationForm
   attributes :title, :description, :main_photo, :place, :creator_id, :begin_date, :end_date, :activity_line_id, :organizer_type, :organizer_id, required: true
 
   association :registrations do
-    attributes :user_id, :event_id, :role, required: true
+    attributes :user_id, :event_id, :role
   end
 end
