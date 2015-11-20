157d26f026a34ce44837fd350fc47b9aaac6d540
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2015-11-20 11:40:35 +0300

    update team title logic

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,8 +6,6 @@ class Team < ActiveRecord::Base
   has_many :events, as: :organizer,
                     foreign_key: :organizer_id
 
-  validates :title, presence: true
-
   include TeamScopes
   extend Enumerize
 
