580ae5a7c8fc352add0e51c517bf6547bbf4453f
pavel <kalashnikovisme@gmail.com>
2015-12-15 04:09:09 +0300

    #refactoring remove empty test files

--- test/decorators/activity_line_decorator_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ActivityLineDecoratorTest < Draper::TestCase
-end
--- test/decorators/activity_lines/corporative/confession_decorator_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ActivityLines::Corporative::ConfessionDecoratorTest < Draper::TestCase
-end
