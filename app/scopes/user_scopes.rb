1cee38c2752dbdfebd5107576244c4cdbf01219d
pavel <kalashnikovisme@gmail.com>
2016-01-09 16:53:38 +0300

    #430 fix user scope

--- app/scopes/user_scopes.rb
+++ app/scopes/user_scopes.rb
@@ -5,6 +5,6 @@ module UserScopes
   included do
     scope :admins, -> { where role: :admin }
     scope :presented, -> { where.not(state: :removed).order('id DESC') }
-    scope :unviewed, -> { where(state: :unviewed, type: nil).where.not(email: nil, state: :removed).order('id DESC') }
+    scope :unviewed, -> { where(state: :unviewed, type: nil).where.not(email: nil).order('id DESC') }
   end
 end
