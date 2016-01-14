6d08eb8b8026952fedd20aa1743cad1517d6aaed
pavel <kalashnikovisme@gmail.com>
2016-01-15 01:18:57 +0300

    #367 fix navbar button

--- lib/dates_helper.rb
+++ lib/dates_helper.rb
@@ -15,11 +15,22 @@ module DatesHelper
   end
 
   def voting_petitions_during?
-    DateTime.now > CONFESSION_DATES.begining_voting &&
-    DateTime.now < CONFESSION_DATES.ending_voting
+    voting_petitions_begins? && !voting_petitions_ends?
+  end
+
+  def voting_petitions_begins?
+    DateTime.now > CONFESSION_DATES.begining_voting
+  end
+
+  def voting_petitions_ends?
+    DateTime.now > CONFESSION_DATES.ending_voting
   end
 
   def current_confession_year
     CONFESSION_DATES.begining_submissions_petitions.year
+  end
+
+  def confession_events_during?
+    submissions_petitions_begins? && !voting_petitions_ends?
   end
 end
