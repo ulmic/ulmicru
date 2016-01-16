10712fdae99f2a6ee76d9869353d2b987b06bf63
pavel <kalashnikovisme@gmail.com>
2016-01-16 18:35:28 +0300

    #FIX test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
-if ENV["TRAVIS"]
+if ENV['TRAVIS']
   require 'coveralls'
   Coveralls.wear!
 end
 
 require 'simplecov'
-ENV["RAILS_ENV"] = "test"
-SimpleCov.start('rails') if ENV["COVERAGE"]
+ENV['RAILS_ENV'] = 'test'
+SimpleCov.start('rails') if ENV['COVERAGE']
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -20,7 +20,7 @@ class ActiveSupport::TestCase
   include Concerns::TechinalPagesManagment
 
   def load_fixture(filename)
-    template = ERB.new(File.read(File.dirname(__FILE__) + "/fixtures/#{filename}"), nil, "%")
+    template = ERB.new(File.read(File.dirname(__FILE__) + '/fixtures/#{filename}'), nil, '%')
     template.result
   end
 end
