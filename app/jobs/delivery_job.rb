c659343808716b2f01aa56a577d0a87f80d03ed7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-02-21 23:03:48 +0300

    #254 sending emails

--- app/jobs/delivery_job.rb
+++ app/jobs/delivery_job.rb
@@ -1,6 +1,10 @@
 class DeliveryJob < ActiveJob::Base
   queue_as :default
 
-  def perform(emails, title, text, link)
+  def perform(emails, title, text, link, image)
+    user = Struct.new email: contact.email, first_name: contact.first_name, last_name: contact.last_name 
+    emails.each do |email|
+      UserMailer.delay.send(user, title, text, link, image)
+    end
   end
 end
