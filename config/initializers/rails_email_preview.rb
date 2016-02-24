42a77ca95517797353f3971aae85c8d4d32567d2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-02-25 02:57:15 +0300

    #254 fix tests with email preview

--- config/initializers/rails_email_preview.rb
+++ config/initializers/rails_email_preview.rb
@@ -23,16 +23,18 @@ require 'rails_email_preview'
 # # enable comfortable_mexican_sofa integration:
 # require 'rails_email_preview/integrations/comfortable_mexica_sofa'
 
-Rails.application.config.to_prepare do
-  # Render REP inside a custom layout (set to 'application' to use app layout, default is REP's own layout)
-  # This will also make application routes accessible from within REP:
-  # RailsEmailPreview.layout = 'admin'
+unless Rails.env.test?
+  Rails.application.config.to_prepare do
+    # Render REP inside a custom layout (set to 'application' to use app layout, default is REP's own layout)
+    # This will also make application routes accessible from within REP:
+    # RailsEmailPreview.layout = 'admin'
 
-  # Set UI locale to something other than :en
-  # RailsEmailPreview.locale = :de
+    # Set UI locale to something other than :en
+    # RailsEmailPreview.locale = :de
 
-  # Auto-load preview classes from:
-  RailsEmailPreview.preview_classes = Dir[Rails.root.join 'app/mailer_previews/*_preview.rb'].map { |p|
-    File.basename(p, '.rb').camelize
-  }
+    # Auto-load preview classes from:
+    RailsEmailPreview.preview_classes = Dir[Rails.root.join 'app/mailer_previews/*_preview.rb'].map { |p|
+      File.basename(p, '.rb').camelize
+    }
+  end
 end
