5ab0397a8077c89835ed4827c1202247e64c54f7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-03-22 01:11:16 +0300

    #503 try to fix

--- app/scopes/questionary_scopes.rb
+++ app/scopes/questionary_scopes.rb
@@ -7,6 +7,7 @@ module QuestionaryScopes
   included do
     scope :unviewed, -> { where(member_state: :unviewed).order('id DESC') }
     scope :presented, -> { where.not(state: :removed) }
-    scope :on_the_trial, -> { where member_state: :on_the_trial }
+    scope :member_on_the_trial, -> { where member_state: :on_the_trial }
+    scope :member_trial_passed, -> { where member_state: :trial_passed }
   end
 end
