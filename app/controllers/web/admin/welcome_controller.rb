808604d9bac78d962715ad0f54325abe9a531b9d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-03-25 00:39:40 +0300

    #503 tabs for trash and welcome

--- app/controllers/web/admin/welcome_controller.rb
+++ app/controllers/web/admin/welcome_controller.rb
@@ -1,4 +1,6 @@
 class Web::Admin::WelcomeController < Web::Admin::ApplicationController
+  skip_before_filter :check_declared_scopes
+  skip_before_filter :collections_counts
   def index
   end
 end
