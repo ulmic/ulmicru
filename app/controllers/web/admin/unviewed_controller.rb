d82cb1ea6af4e20326b68f4d2ba9c9d44a23d3c4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-03-25 00:37:22 +0300

    #503 tabs to unviewed

--- app/controllers/web/admin/unviewed_controller.rb
+++ app/controllers/web/admin/unviewed_controller.rb
@@ -1,9 +1,30 @@
 class Web::Admin::UnviewedController < Web::Admin::ApplicationController
+  skip_before_filter :check_declared_scopes
+  skip_before_filter :collections_counts
+  before_filter :check_notificatable_items
+  before_filter :unviewed_counts
+
   def index
-    @unviewed = {}
-    Concerns::NotificatableItems.items.each do |item|
-      @unviewed[item.to_s.pluralize(:en).to_sym] = item.to_s.camelize.constantize.unviewed.page(params[:page]).decorate
-    end
+    @unviewed = params[:items].to_s.classify.constantize.unviewed.page(params[:page]).decorate
     @tag = Tag.new
+  end
+
+  private
+
+  def check_notificatable_items
+    if params[:items].present?
+      unless Concerns::NotificatableItems.items.include? params[:items].to_sym
+	redirect_to params.except(:items)
+      end
+    else
+      params[:items] ||= Concerns::NotificatableItems.items.first
+    end
+  end
+
+  def unviewed_counts
+    @counts = {}
+    Concerns::NotificatableItems.items.each do |item|
+      @counts[item] = item.to_s.classify.constantize.unviewed.count 
+    end
   end
 end
