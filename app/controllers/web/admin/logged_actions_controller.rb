bf74aeb06c4441cf7206bb245d6bedb2330c4194
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-04-25 00:16:32 +0400

    #400 some fixes

--- app/controllers/web/admin/logged_actions_controller.rb
+++ app/controllers/web/admin/logged_actions_controller.rb
@@ -8,7 +8,7 @@ class Web::Admin::LoggedActionsController < Web::Admin::ApplicationController
     else
       logged_actions = LoggedAction.all
     end
-    @logged_actions = logged_actions.page(params[:page]).decorate
+    @logged_actions = logged_actions.order('created_at DESC').page(params[:page]).decorate
   end
 
   def show
