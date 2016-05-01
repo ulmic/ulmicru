c8ae78f2f95e6dfc6f4f814ea3f57d34ec0864f9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-05-01 16:17:17 +0400

    #144 more statistics

--- app/controllers/web/admin/welcome_controller.rb
+++ app/controllers/web/admin/welcome_controller.rb
@@ -7,8 +7,9 @@ class Web::Admin::WelcomeController < Web::Admin::ApplicationController
     @report = { 
       users_count: User.count,
       event_registrations_count: Event::Registration.count,
+      uniq_users_event_registrations_count: Event::Registration.all.map(&:user).uniq.count,
       uniq_logged_users_for_month_count: LoggedAction.where(action_type: :sign_in).
-	where('created_at > ?', DateTime.now - 1.month).uniq { |l| l.user_id }.count,
+	where('created_at > ?', DateTime.now - 1.month).map(&:user).uniq.count,
       new_users_count: User.where('created_at > ?', DateTime.now - 1.month).count,
       news_views: News.all.map(&:views).sum
     }
