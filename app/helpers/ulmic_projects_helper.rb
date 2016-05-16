c9b6c73fc4591abc7e12584cb376be2cd6527e62
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-05-16 04:11:38 +0400

    #REFACTOR ulmic_projects_helper

--- app/helpers/ulmic_projects_helper.rb
+++ app/helpers/ulmic_projects_helper.rb
@@ -1,21 +1,13 @@
 module UlmicProjectsHelper
 
   ### Navbar links
-
-  def ul_lider_link
-    link_to configus.it_projects.ul_lider.url, class: :blank do
+  
+  def ulmic_project_link(project)
+    project_config = configus.it_projects.send project
+    link_to project_config.url, class: :blank do
       content_tag :div, class: :inner do
-        concat image_tag 'organization/activity_lines/lider/ya-lider.png', class: 'custom-icon'
-        concat content_tag :span, " #{t('organization.projects.i_am_lider')}"
-      end
-    end
-  end
-
-  def it_way_link
-    link_to configus.it_projects.it_way.url, class: :blank do
-      content_tag :div, class: :inner do
-        concat image_tag 'organization/activity_lines/it/it_way.png', class: 'custom-icon'
-        concat content_tag :span, " #{t('organization.projects.it_way')}'16"
+        concat image_tag project_config.ico, class: 'custom-icon'
+        concat content_tag :span, " #{project_config.short_name}"
       end
     end
   end
