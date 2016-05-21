bbcb087414704286a74ed0bdb28973c09a4c5d1d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-05-22 02:10:06 +0400

    #570 some success in logging system

--- lib/concerns/params_comparing_concern.rb
+++ lib/concerns/params_comparing_concern.rb
@@ -13,7 +13,8 @@ module Concerns
     end
 
     def attributes_diff(object_hash, params, prev_object_hash)
-      if comparing(object_hash, params) == {}
+      binding.pry
+      if comparing(object_hash, params) == []
         comparing object_hash, prev_object_hash, :prev_object
       end
     end
@@ -33,31 +34,23 @@ module Concerns
     end
 
     def comparing(hash1, hash2, prev_object = nil)
-      comparison = {}
-      hash1.except('updated_at').each do |key, value|
-        next if !hash2[key].present? && !prev_object
-        if value.is_a? ActiveSupport::TimeWithZone
-          if hash2[key].to_datetime.to_s.include? "+00"
-            hash2[key] = (hash2[key].to_datetime - 3.hour).in_time_zone('Moscow')
-          else
-            hash2[key] = hash2[key].to_datetime.in_time_zone('Moscow')
-          end
-        end
-        if value.is_a? Hash
-          # because of nested values hash has one value (association_attributes)
-	  if hash2[key]
-	    comparison[key] = comparing hash2[key].values.first, value.values.first
-	  end
-          next
-        end
-        unless hash2[key].to_s == value.to_s
-          if value.is_a? Hash
-            value.except!(*not_logged_attributes)
-          end
-          comparison[key] = value
+      hash1.except!('id', 'updated_at')
+      hash1.each do |key, value|
+        unless value.is_a? String
+          hash1[key] = value.to_s
         end
       end
-      comparison
+      hash2.each do |key, value|
+        if value.include?('/') && Date.valid_date?(*value.split('/').reverse.map(&:to_i))
+          if value.to_datetime.to_s.include?("+00")
+            hash2[key] = (hash2[key].to_datetime - 3.hour).in_time_zone('Moscow').to_s
+          end
+        end
+      end
+      HashDiff.diff hash1, hash2
+    end
+
+    def recursive_to_s(hash)
     end
 
     def to_param(model_class_name)
