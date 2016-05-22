43b873ba5f7e262792e9fb5e7c7c241573674997
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-05-22 20:21:45 +0400

    #570 view of registration_with_logs

--- app/controllers/concerns/logged_actions_params_concern.rb
+++ app/controllers/concerns/logged_actions_params_concern.rb
@@ -5,5 +5,13 @@ module Concerns
         (attributes.to_a - hash.to_a).empty?
       end&.include? true
     end
+
+    def added_nested_attribute?(param, association_name)
+      param.first == '+' && param[1].split('.')[0] == association_name.to_s
+    end
+
+    def added_nested_attribute_id(param)
+      param[1].split('.')[1].to_i
+    end
   end
 end
