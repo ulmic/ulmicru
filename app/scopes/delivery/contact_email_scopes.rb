e94ee4341453b6d63432268ea28d75070f94ca66
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-07-03 10:43:32 +0400

    #254 update contact_email logic

--- app/scopes/delivery/contact_email_scopes.rb
+++ app/scopes/delivery/contact_email_scopes.rb
@@ -1,13 +1,10 @@
 require 'scopes_rails/state_machine/scopes'
 
-module UserScopes
+module Delivery::ContactEmailScopes
   extend ActiveSupport::Concern
   include StateMachine::Scopes
 
   included do
-    scope :admins, -> { where role: :admin }
-    scope :presented, -> { where.not(state: :removed).order('id DESC') }
-    scope :unviewed, -> { where(state: :unviewed, type: nil).where.not(email: nil).order('id DESC') }
     scope :subscribed_to_deliveries, -> { joins(:subscriptions).where 'subscriptions.subscription_type = ?', :deliveries }
   end
 end
