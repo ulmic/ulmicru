b56cca289a4fc0e466bf7ec1f1f1071420785de1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-07-24 04:52:05 +0400

    #582 fix questionary_mailer

--- app/mailers/questionary_mailer.rb
+++ app/mailers/questionary_mailer.rb
@@ -1,7 +1,7 @@
 class QuestionaryMailer < ApplicationMailer
   default template_path: "mailers/#{self.name.underscore}"
 
-  def create(news, user)
+  def create(questionary, user)
     @user = user
     @questionary = questionary
     mail from: sender_name, to: user.email, subject: subject(questionary.class, :create)
