c394c9fd6f693c4c2b217c7a9e9e18877e27c5b9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-08-04 02:57:10 +0400

    #583 many updates for contact_list

---
+++ lib/organization/contact_list.rb
@@ -0,0 +1,21 @@
+module Organization
+  module ContactList
+    def self.list
+      teams.reduce([]) do |members, team|
+        members += MemberDecorator.decorate_collection(team.members.includes(:positions).map do |member|
+          member if member.positions.current_positions.any?
+        end.compact)
+      end
+    end
+
+    private
+
+    class << self
+      include Organization::Teams
+
+      def teams
+        [ presidium, area_headers ] + Team::Departament.presented
+      end
+    end
+  end
+end
