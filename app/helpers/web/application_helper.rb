981f1c68fe67e626f8568614a4401f5b058ba47d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-08-09 03:47:49 +0400

    #583 update hint function

--- app/helpers/web/application_helper.rb
+++ app/helpers/web/application_helper.rb
@@ -21,4 +21,21 @@ module Web::ApplicationHelper
   def javascript_include_tag_if_exists(link)
     javascript_include_tag link if Ulmicru::Application.assets.find_asset link
   end
+
+  def hint(model_name, attribute_name, place:)
+    case place
+    when :admin
+      content_tag :div, class: 'alert alert-dissmissible alert-info' do
+        concat fa_icon 'info-circle'
+        concat ' '
+        concat t("hints.admin.#{model_name.to_s.underscore}.#{attribute_name}")
+      end
+    when :face
+      content_tag :div, class: 'panel panel-info' do
+        concat fa_icon 'info-circle'
+        concat ' '
+        concat t("hints.#{model_name.to_s.underscore}.#{attribute_name}")
+      end
+    end
+  end
 end
