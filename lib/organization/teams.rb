c77bc9c9ab37ac1652922029ff472133b31c905e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-08-09 04:43:46 +0400

    #583 updates for positiosn

--- lib/organization/teams.rb
+++ lib/organization/teams.rb
@@ -11,16 +11,15 @@ module Organization
     def positions_associate_with_team(positions, team)
       positions_structure ||= YAML.load_file("#{Rails.root}/lib/yaml/positions.yml").with_indifferent_access
       team_positions = positions_structure[:positions][:team].map do |position|
-        case position.class
-        when String
-          "#{position.capitalize.to_s} #{team.full_title(:genitive)}"
-        when Hash
-          deputy = p[:deputy]
+        if position.is_a? String
+          "#{position.mb_chars.capitalize.to_s} #{team.full_title(:genitive)}"
+        elsif position.is_a? Hash
+          deputy = position[:deputy]
           deputy.map do |name|
-            "Заместитель #{genitive(name)} #{tean.full_title(:genitive)}"
+            "Заместитель #{genitive(name)} #{team.full_title(:genitive)}"
           end
         end
-      end.flatten
+      end
       positions.where title: team_positions
     end
   end
