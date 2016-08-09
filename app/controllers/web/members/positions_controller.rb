415309b6775a39a49120334b561edf03de91b680
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-08-09 05:33:06 +0400

    #583 add positions form

--- app/controllers/web/members/positions_controller.rb
+++ app/controllers/web/members/positions_controller.rb
@@ -1,6 +1,7 @@
 class Web::Members::PositionsController < Web::Members::ApplicationController
   def edit
-    @position = PositionForm.find_with_model params[:id]
+    @position_form = PositionForm.find_with_model params[:id]
+    @position = Position.includes(:member).find params[:id]
   end
 
   def update
