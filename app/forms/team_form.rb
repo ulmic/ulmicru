c33b964547fb23ebaa721b3a231dc2f14a01af44
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-03 14:36:26 +0400

    #FIX team form

--- app/forms/team_form.rb
+++ app/forms/team_form.rb
@@ -1,3 +1,3 @@
 class TeamForm < ApplicationReform
-  properties :title, :member_id, :description, :state, :user_ids
+  properties :title, :member_id, :description, :state, :user_ids, :school, :municipality, :type, :team_id, :publicity
 end
