1a139da6ec056ef236a175fb738b6e3213059d16
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-09 15:59:48 +0400

    #UPDATE add icons to tabs

--- app/helpers/web/admin/icons_helper.rb
+++ app/helpers/web/admin/icons_helper.rb
@@ -2,8 +2,21 @@ module Web::Admin::IconsHelper
   ICONS = {
     removed: :remove,
     closed: :lock,
-    unviewed: 'info-circle',
-    active: :fire
+    unviewed: :inbox,
+    active: :asterisk,
+    future: 'arrow-circle-o-right',
+    past: 'arrow-circle-o-left',
+    current: 'dot-circle-o',
+    declined: :ban,
+    main: :fire,
+    confirmed: :asterisk,
+    unavailable: 'user-times',
+    member_on_the_trial: 'clock-o',
+    member_trial_passed: 'thumbs-up',
+    sended: 'paper-plane',
+    on_vote: 'hand-paper-o',
+    inactive: 'eye-slash',
+    actual: 'calendar-check-o'
   }
   
   def fa_icon_by_state(state)
