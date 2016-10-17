0daa99205c4a49f7ccfec2fb73a9445ca0e03241
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-18 02:59:54 +0400

    #TEST fix

--- test/controllers/web/articles_controller_test.rb
+++ test/controllers/web/articles_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Web::ArticlesControllerTest < ActionController::TestCase
   setup do
     @category = create :category
-    @article = create :article
+    @article = create :article, access: :all
     create :article, :contacts
   end
 
