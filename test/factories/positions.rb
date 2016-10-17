e6847da277b13b5a13646d59a3a45d8a8f7469e4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-18 02:52:07 +0400

    #TEST fix

--- test/factories/positions.rb
+++ test/factories/positions.rb
@@ -15,4 +15,9 @@ FactoryGirl.define do
   factory :position_deputy_corporative_lead, parent: :position_corporative_lead do
     title 'Заместитель руководителя корпоративных проектов'
   end
+
+  factory :position_press_center_head, parent: :position do
+    title 'Руководитель пресс-центра'
+    for_now 1
+  end
 end
