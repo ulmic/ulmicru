2cf012f1346a69701848dd4cdc06807029e9a814
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-25 03:30:29 +0400

    #UPDATE many permissiosn

--- app/controllers/concerns/notificatable_items.rb
+++ app/controllers/concerns/notificatable_items.rb
@@ -3,8 +3,8 @@ module Concerns
     def self.items(current_user_id)
       types = [ :member, :questionary, :news, :event, :feedback, :comment, :article, :position, :team ]
       types.map do |type|
-        type if Organization::Permissions.send(type)[:need_to_review].include?(current_user_id)
-      end.concat
+        type if Organization::Permissions.send(type)[:need_to_review].map(&:id).include?(current_user_id)
+      end.compact
     end
   end
 end
