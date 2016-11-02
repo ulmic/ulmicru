b910ced4bb2795d6d54b7ac8df860262325e8d3e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-11-03 03:15:42 +0400

    #FIX admin campaign view

--- app/decorators/delivery/campaign_decorator.rb
+++ app/decorators/delivery/campaign_decorator.rb
@@ -14,4 +14,28 @@ class Delivery::CampaignDecorator < ApplicationDecorator
   def name
     model.title
   end
+
+  def view_attributes
+    attrs = [:title, :body, :image_tag, :link_to, :creator_link, :mailing_date, :created_at, :updated_at,
+     :state, :letters_count]
+    attrs.reduce({}) do |hash, attr|
+      hash.merge! attr => send(attr)
+    end
+  end
+
+  def image_tag
+    h.image_tag image
+  end
+
+  def link_to
+    h.link_to link, link
+  end
+
+  def creator_link
+    h.link_to object.creator.decorate.short_name, admin_member_path(object.creator)
+  end
+
+  def letters_count
+    object.contacts.count
+  end
 end
