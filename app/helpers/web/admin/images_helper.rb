6653f39bd320231899bdd9c4c1ce46a41a97bd26
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-11-07 00:14:27 +0400

    #FIX

--- app/helpers/web/admin/images_helper.rb
+++ app/helpers/web/admin/images_helper.rb
@@ -1,7 +1,7 @@
 module Web::Admin::ImagesHelper
   def new_redirect_rule_by_image_path(image)
     new_admin_redirect_rule_path(redirect_rule: {
-      redirect_to: @image_form.file.url,
+      redirect_to: image.file.url,
       status: :moved_permanently
     })
   end
