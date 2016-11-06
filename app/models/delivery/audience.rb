f079f30bd9460c52e40d924d6c5200bda6f1a483
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-11-06 22:23:06 +0400

    #554 search around own attributes

--- app/models/delivery/audience.rb
+++ app/models/delivery/audience.rb
@@ -7,25 +7,23 @@ class Delivery::Audience < ActiveRecord::Base
   enumerize :audience_type, in: [ :team, :users, :contact_emails, :event_registrations, :members ], default: :users
 
   def contacts
-    contacts = []
     case audience_type
     when 'users'
-      contacts = User.subscribed_to_deliveries.with_email
+      User.presented.subscribed_to_deliveries.with_email
     when 'members'
-      contacts = Member.subscribed_to_deliveries.with_email
+      Member.presented.subscribed_to_deliveries.with_email
     when 'contacts_emails'
-      [User, ContactEmail].each do |type|
-        type.subscribed_to_deliveries.with_email.find_each(batch_size: 1000) do |instance|
+      [User, ContactEmail].reduce([]) do |contacts, type|
+        type.presented.subscribed_to_deliveries.with_email.find_each(batch_size: 1000) do |instance|
           contacts << OpenStruct.new(email: instance.email, first_name: instance.first_name, last_name: instance.last_name)
         end
       end
     when 'team'
       team = Team.find audience_id
-      contacts = team.users.subscribed_to_deliveries.with_email
+      team.users.presented.subscribed_to_deliveries.with_email
     when 'event_registrations'
       event = Event.find audience_id
-      contacts = User.subscribed_to_deliveries.with_email.where id: event.registrations.map(&:user_id)
+      User.presented.subscribed_to_deliveries.with_email.where id: event.registrations.map(&:user_id)
     end
-    contacts
   end
 end
