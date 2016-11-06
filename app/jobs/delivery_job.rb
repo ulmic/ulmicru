c93b72aff4eb5a4ad7f492eeee31649d1f3afe8b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-11-06 17:09:39 +0400

    #FIX delivery

--- app/jobs/delivery_job.rb
+++ app/jobs/delivery_job.rb
@@ -6,7 +6,12 @@ class DeliveryJob < ActiveJob::Base
       combination.each do |contact|
         UserMailer.delay.send :just_message, contact, title, text, link, image, contact.subscribe_token.content
       end
-      sleep 2
+      #NOTE for testing
+      if Rails.env.production?
+        sleep 60
+      else
+        sleep 2
+      end
     end
   end
 end
