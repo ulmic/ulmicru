750d10289709ee3dfc3482b659f8b5fcdde1ecaf
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-11-18 03:45:49 +0400

    #608 fix petitions_controller

--- app/controllers/web/members/corporative/petitions_controller.rb
+++ app/controllers/web/members/corporative/petitions_controller.rb
@@ -1,37 +1,28 @@
 class Web::Members::Corporative::PetitionsController < Web::Members::Corporative::ApplicationController
   before_filter :choose_members, only: [ :new, :edit ]
+  before_filter :check_submission_dates
 
   include DatesHelper
 
   def new
-    if !submissions_petitions_begins?
-      redirect_to not_found_page_path
-    elsif submissions_petitions_ends?
-      redirect_to page_page_path :confession_submissions_petitions_ends
-    else
-      @petition_form = ::ActivityLines::Corporative::ConfessionForm.new_with_model
-      @petition_form.build_arguments_for_petition
-    end
+    @petition_form = ::ActivityLines::Corporative::ConfessionForm.new_with_model
+    @petition_form.build_arguments_for_petition
   end
 
   def create
-    if submissions_petitions_during?
-      confession_params = params[:activity_lines_corporative_confession]
-      confession_params[:year] = current_confession_year
-      confession_params[:creator_id] = current_user.id
-      confession_params[:arguments_attributes].keys.each do |index|
-        confession_params[:arguments_attributes][index][:member_id] = current_user.id
-      end
-      params[:activity_lines_corporative_confession] = confession_params
-      @petition_form = ::ActivityLines::Corporative::ConfessionForm.new_with_model
-      if @petition_form.submit params[:activity_lines_corporative_confession]
-        redirect_to activity_lines_corporative_petitions_path
-      else
-        choose_members
-        render :new
-      end
+    confession_params = params[:activity_lines_corporative_confession]
+    confession_params[:year] = current_confession_year
+    confession_params[:creator_id] = current_user.id
+    confession_params[:arguments_attributes].keys.each do |index|
+      confession_params[:arguments_attributes][index][:member_id] = current_user.id
+    end
+    params[:activity_lines_corporative_confession] = confession_params
+    @petition_form = ::ActivityLines::Corporative::ConfessionForm.new_with_model
+    if @petition_form.submit params[:activity_lines_corporative_confession]
+      redirect_to activity_lines_corporative_petitions_path
     else
-      redirect_to not_found_page_path
+      choose_members
+      render :new
     end
   end
 
@@ -46,6 +37,16 @@ class Web::Members::Corporative::PetitionsController < Web::Members::Corporative
       end
     else
       redirect_to not_found_page_path
+    end
+  end
+
+  private
+
+  def check_submission_dates
+    if !submissions_petitions_begins?
+      redirect_to not_found_page_path
+    elsif submissions_petitions_ends?
+      redirect_to page_page_path :confession_submissions_petitions_ends
     end
   end
 end
