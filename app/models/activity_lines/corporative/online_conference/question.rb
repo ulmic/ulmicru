438e6452a83cebc92ec76b800d13161e57db8f0d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-06 00:59:35 +0400

    #UPDATE add state_machine to question

--- app/models/activity_lines/corporative/online_conference/question.rb
+++ app/models/activity_lines/corporative/online_conference/question.rb
@@ -1,3 +1,16 @@
 class ActivityLines::Corporative::OnlineConference::Question < ActiveRecord::Base
   belongs_to :online_conference, class_name: 'ActivityLines::Corporative::OnlineConference'
+
+  state_machine :state, initial: :unviewed do
+    state :unviewed
+    state :active
+    state :removed
+
+    event :remove do
+      transition all => :removed
+    end 
+    event :restore do
+      transition all => :active
+    end
+  end
 end
