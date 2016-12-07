65b8ad4f014f9edf607bdf21df9b417edd7b6e8c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-07 04:38:01 +0400

    #UPDATE ask question as anonym

--- app/controllers/web/activity_lines/corporative/online_conferences/questions_controller.rb
+++ app/controllers/web/activity_lines/corporative/online_conferences/questions_controller.rb
@@ -1,6 +1,8 @@
 class Web::ActivityLines::Corporative::OnlineConferences::QuestionsController < 
       Web::ActivityLines::Corporative::ApplicationController
 
+  before_filter :check_anonym
+
   def create
     question = ::ActivityLines::Corporative::OnlineConference::QuestionForm.new_with_model
     if question.submit params[:activity_lines_corporative_online_conference_question]
@@ -8,5 +10,16 @@ class Web::ActivityLines::Corporative::OnlineConferences::QuestionsController <
     else
       redirect_to question.online_conference.event
     end
+  end
+
+  private
+
+  def check_anonym
+    user_id = params[:activity_lines_corporative_online_conference_question][:user_id]
+    params[:activity_lines_corporative_online_conference_question][:user_id] = if user_id == '0'
+        current_user.id
+      else
+        nil
+      end
   end
 end
--- app/forms/activity_lines/corporative/online_conference/question_form.rb
+++ app/forms/activity_lines/corporative/online_conference/question_form.rb
@@ -1,3 +1,5 @@
 class ActivityLines::Corporative::OnlineConference::QuestionForm < ApplicationReform
   properties :text, :user_id, :online_conference_id
+
+  attr_accessor :anonym
 end
