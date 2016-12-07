c5ad5c84564e4b5b18a8a99ab09fa874da2653a1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-07 15:58:21 +0400

    #UPDATE update answer on question

--- app/controllers/web/admin/activity_lines/corporative/online_conference/questions_controller.rb
+++ app/controllers/web/admin/activity_lines/corporative/online_conference/questions_controller.rb
@@ -1,10 +1,14 @@
 class Web::Admin::ActivityLines::Corporative::OnlineConference::QuestionsController < 
       Web::Admin::ActivityLines::Corporative::ApplicationController
 
+  def edit
+    @question_form = ::ActivityLines::Corporative::OnlineConference::QuestionForm.find_with_model params[:id]
+  end
+
   def update
     @question = ::ActivityLines::Corporative::OnlineConference::QuestionForm.find_with_model params[:id]
     if @question.submit params[:activity_lines_corporative_online_conference_question]
-      redirect_to admin_unviewed_index_path items: 'activity_lines/corporative/online_conference/question'
+      redirect_to :back
     end
   end
 
--- app/decorators/activity_lines/corporative/online_conference/question_decorator.rb
+++ app/decorators/activity_lines/corporative/online_conference/question_decorator.rb
@@ -3,9 +3,9 @@ class ActivityLines::Corporative::OnlineConference::QuestionDecorator < Applicat
 
   def answer_link
     if object.answer_timestamp.present?
-      minute = object.answer_timestamp.match(/\d:/)[0..-2]
-      second = object.answer_timestamp.match(/:\d/)[1..-1]
-      "#{object.video_link}?t=#{minute}m#{second}s"
+      minute = object.answer_timestamp.match(/\d+:/).to_s[0..-2]
+      second = object.answer_timestamp.match(/:\d+/).to_s[1..-1]
+      "#{object.online_conference.video_link}?t=#{minute}m#{second}s"
     end
   end
 
