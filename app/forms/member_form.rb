4302dd7d50ae2e0fa561545e8fda2b0a111dede5
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-12 00:33:04 +0400

    #FIX add join date to form

--- app/forms/member_form.rb
+++ app/forms/member_form.rb
@@ -2,7 +2,7 @@ class MemberForm < ApplicationForm
   self.main_model = :member
 
   attributes :first_name, :last_name, :patronymic, :email, :motto, :ticket, :mobile_phone, :birth_date, :home_adress, :municipality, :locality, :avatar, :state, :type, required: true
-  attributes :state_event, :parent_id, :school, :password, :corporate_email
+  attributes :state_event, :parent_id, :school, :password, :corporate_email, :join_date
 
   association :positions do
     attributes :title, :begin_date, :member_id, :state, :end_date, :for_now, :status
