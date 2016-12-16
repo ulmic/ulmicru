b5e9c421e0e1809f0f1e909b2312c174ea7779d5
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-17 02:30:02 +0400

    #616 update auth

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -4,10 +4,7 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    add_foreign_key :table_name, :users, column: :resource_owner_id
-    # Put your resource owner authentication logic here.
-    # Example implementation:
-    #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    User.find_by_id(session[:current_user_id]) || redirect_to(new_session_path)
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
