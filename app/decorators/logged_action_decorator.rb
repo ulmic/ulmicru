509d5527623a8f4b36f2f63a06483650e1d94849
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-22 18:09:03 +0400

    #FIX add updated_params to logged_actions

--- app/decorators/logged_action_decorator.rb
+++ app/decorators/logged_action_decorator.rb
@@ -8,4 +8,12 @@ class LoggedActionDecorator < ApplicationDecorator
   def user_short_name
     object.user.decorate.short_name
   end
+
+  def updated_params
+    if object.action_type == 'update'
+      object.parsed_params.map do |param|
+        "#{'Изменено:' if param[0] == '~'} #{t("activerecord.attributes.#{object.record_type.underscore}.#{param[1]}")} c '#{param[2]}' на '#{param[3]}'"
+      end
+    end
+  end
 end
