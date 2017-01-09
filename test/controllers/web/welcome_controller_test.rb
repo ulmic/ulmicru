a618256ee5d4e50b72aa3e12f91fa80c9f64d35f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-10 01:33:23 +0400

    #TEST 10 times test welcome page

--- test/controllers/web/welcome_controller_test.rb
+++ test/controllers/web/welcome_controller_test.rb
@@ -2,8 +2,10 @@ require 'test_helper'
 
 class Web::WelcomeControllerTest < ActionController::TestCase
   test 'should get index' do
-    get :index
-    assert_response :success, @response.body
+    10.times do
+      get :index
+      assert_response :success, @response.body
+    end
   end
 
   test 'should get index with auth' do
