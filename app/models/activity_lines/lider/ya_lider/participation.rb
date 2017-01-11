b526d251e3e98a842dce988674e3870caceb6082
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-11 04:51:12 +0400

    #617 add associations

--- app/models/activity_lines/lider/ya_lider/participation.rb
+++ app/models/activity_lines/lider/ya_lider/participation.rb
@@ -1,2 +1,4 @@
 class ActivityLines::Lider::YaLider::Participation < ActiveRecord::Base
+  belongs_to :participant, class_name: 'ActivityLines::Lider::YaLider::Participant'
+  belongs_to :stage, class_name: 'ActivityLines::Lider::YaLider::Stage'
 end
--- app/models/activity_lines/lider/ya_lider/stage.rb
+++ app/models/activity_lines/lider/ya_lider/stage.rb
@@ -1,5 +1,7 @@
 class ActivityLines::Lider::YaLider::Stage < ActiveRecord::Base
   belongs_to :contest, class_name: 'ActivityLines::Lider::YaLider'
+  has_many :participations, class_name: 'ActivityLines::Lider::YaLider::Participation'
+  has_many :participants, through: :participations
 
   validates :number, presence: true, uniqueness: { scope: :ya_lider_id }
 
