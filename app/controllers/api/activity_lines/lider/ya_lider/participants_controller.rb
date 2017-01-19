219134edf1ac8ffb111414e3e9381923027d6472
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-19 08:10:39 +0400

    #617 create participant via API with existed user

--- app/controllers/api/activity_lines/lider/ya_lider/participants_controller.rb
+++ app/controllers/api/activity_lines/lider/ya_lider/participants_controller.rb
@@ -4,12 +4,17 @@ class Api::ActivityLines::Lider::YaLider::ParticipantsController < Api::Activity
   def create
     user = User.confirmed.where(email: params[:user][:email]).first
     if user
-      user.update_attributes! params[:user]
+      user_form = UserForm.new user
+      params[:user].each do |attr, value|
+        if user.try(attr).nil?
+          user_form.submit attr => value 
+        end
+      end
       create_participant user
     else
       user_form = ActivityLines::Lider::YaLider::ParticipantUserForm.new User.new
       if user_form.submit params[:user]
-        create_participant user_form
+        create_participant user_form, avatar_update: true
       else
         User.tech_admins.each do |user|
           send_notification user, user_form.model, :fail_participant_api_create
@@ -21,7 +26,7 @@ class Api::ActivityLines::Lider::YaLider::ParticipantsController < Api::Activity
 
   private
 
-  def create_participant(user)
+  def create_participant(user, avatar_update: false)
     params[:activity_lines_lider_ya_lider_participant] = {}
     params[:activity_lines_lider_ya_lider_participant][:user_id] = user.id
     params[:activity_lines_lider_ya_lider_participant][:contest_id] = ::ActivityLines::Lider::YaLider.current.first.id
@@ -30,7 +35,9 @@ class Api::ActivityLines::Lider::YaLider::ParticipantsController < Api::Activity
       first_stage = participant_form.model.contest.first_stage
       ActivityLines::Lider::YaLider::Participation.create! participant_id: participant_form.model.id, stage_id: first_stage.id
       head :ok
-      user.model.update_attributes! remote_avatar_url: "#{configus.it_projects.ul_lider.url}#{params[:user][:avatar]}"
+      if avatar_update
+        user.model.update_attributes! remote_avatar_url: "#{configus.it_projects.ul_lider.url}#{params[:user][:avatar]}"
+      end
     else
       User.tech_admins.each do |user|
         send_notification user, participant_form.model, :fail_api_create
