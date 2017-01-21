18b72e5a606b9e71d15ef4eaa9d47127d53cb294
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-21 10:50:48 +0400

    #575 some updates

--- app/controllers/api/admin/places_controller.rb
+++ app/controllers/api/admin/places_controller.rb
@@ -1,10 +1,22 @@
 class Api::Admin::PlacesController < Api::Admin::ApplicationController
+  skip_before_filter :authenticate_admin!
+  before_filter :authenticate_permitted_create_place!
+
   def create
     @place = Place.new_with_model
     if @place.submit params[:place]
       head :ok
     else
       head :bad_request
+    end
+  end
+
+  private
+
+  def authenticate_permitted_create_place!
+    unless permitted_to? :create, :place
+      head :not_acceptable
+      return
     end
   end
 end
