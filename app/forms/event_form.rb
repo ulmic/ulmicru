3c970da42e4c390a55de97343ffe3ba4579a9e7a
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-21 07:39:40 +0400

    #575 updates

--- app/forms/event_form.rb
+++ app/forms/event_form.rb
@@ -1,7 +1,7 @@
 class EventForm < ApplicationForm
   self.main_model = :event
 
-  attributes :title, :description, :main_photo, :place, :creator_id, :begin_date, :end_date, :activity_line_id, 
+  attributes :title, :description, :main_photo, :place_ids, :creator_id, :begin_date, :end_date, :activity_line_id, 
              :organizer_type, :organizer_id, required: true
 
   association :registrations do
