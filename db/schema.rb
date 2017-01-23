3c26dd8c32beb05820c0ff9260a4dc74cbbe4093
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-23 04:11:05 +0400

    #617 some updates

--- db/schema.rb
+++ db/schema.rb
@@ -90,7 +90,6 @@ ActiveRecord::Schema.define(version: 20170121022820) do
     t.integer  "stage_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.text     "state"
   end
 
   create_table "activity_lines_lider_ya_lider_stages", force: :cascade do |t|
@@ -496,6 +495,9 @@ ActiveRecord::Schema.define(version: 20170121022820) do
     t.integer "team_id"
     t.integer "user_id"
   end
+
+  add_index "teams_users", ["team_id"], name: "index_teams_users_on_team_id", using: :btree
+  add_index "teams_users", ["user_id"], name: "index_teams_users_on_user_id", using: :btree
 
   create_table "test_paper_questions", force: :cascade do |t|
     t.text     "text"
