f2e0a0b564aca4ac62602de56d5664e284fd7d91
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-24 20:41:18 +0400

    #TEST updates

--- test/factories/activity_lines/lider/ya_liders.rb
+++ test/factories/activity_lines/lider/ya_liders.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :ya_lider, class: 'ActivityLines::Lider::YaLider' do
-    contest_number { ActivityLines::Lider::YaLider.order(:contest_number).last.contest_number + 1 }
+    contest_number do
+      if ActivityLines::Lider::YaLider.any?
+        ActivityLines::Lider::YaLider.order(:contest_number).last.contest_number + 1
+      else
+        1
+      end
+    end
     contest_year { generate :integer }
     state { ActivityLines::Lider::YaLider.state_machines[:state].states.map(&:name).first.to_s }
     after :create do
