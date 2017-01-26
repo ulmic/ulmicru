86a0eb93971ba89c1a2440b9f3722bbc988ea692
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-26 04:16:28 +0400

    #617 some fixes

--- test/factories/activity_lines/lider/ya_lider/participants.rb
+++ test/factories/activity_lines/lider/ya_lider/participants.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :ya_lider_participant, class: 'ActivityLines::Lider::YaLider::Participant' do
-    contest_year { generate :integer }
+    contest_id { ActivityLines::Lider::YaLider.last&.id || create(:ya_lider).id }
     user_id { User.last ? User.last.id : create(:user).id }
     #state { ActivityLines::Lider::YaLider::Participant.state_machines[:state].states.map(&:name).first.to_s }
   end
