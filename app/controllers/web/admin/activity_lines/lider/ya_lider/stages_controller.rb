d99d007bc07c108ed27fa72934666783483ff028
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-29 10:15:21 +0400

    #FIX stages

--- app/controllers/web/admin/activity_lines/lider/ya_lider/stages_controller.rb
+++ app/controllers/web/admin/activity_lines/lider/ya_lider/stages_controller.rb
@@ -11,7 +11,7 @@ class Web::Admin::ActivityLines::Lider::YaLider::StagesController < Web::Admin::
   def create
     @stage_form = ::ActivityLines::Lider::YaLider::StageForm.new_with_model
     if @stage_form.submit params[:activity_lines_lider_ya_lider_stage]
-      redirect_to admin_activity_lines_lider_ya_lider_path @stage_form.model.ya_lider
+      redirect_to admin_activity_lines_lider_ya_lider_path @stage_form.model.contest
     else
       render action: :new
     end
@@ -33,6 +33,6 @@ class Web::Admin::ActivityLines::Lider::YaLider::StagesController < Web::Admin::
   def destroy
     @stage = ::ActivityLines::Lider::YaLider::Stage.find params[:id]
     @stage.remove
-    redirect_to admin_activity_lines_lider_ya_lider_path @stage.ya_lider
+    redirect_to admin_activity_lines_lider_ya_lider_path @stage.contest
   end
 end
