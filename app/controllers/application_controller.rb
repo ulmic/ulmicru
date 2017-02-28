6fe25b03d9764a243fc5f4b3cad6a41adae9e12f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-02-28 14:55:21 +0400

    #634 redirect to it_way

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :get_categories
+  before_filter :redirect_to_another_main_page
 
   include Concerns::AuthManagment
   include Concerns::TechinalPagesManagment
   include Concerns::ActionLogger
   include Concerns::ContactsSubscriptionsManagment
+  include Concerns::ItProjectsConcern
   include DatesHelper
 
   private
 
   def redirect_to_another_main_page
-    binding.pry
+    unless request.host == configus.host.split(':')[0]
+      redirect_to main_page_of request.host
+    end
   end
 
   def get_categories
--- app/controllers/concerns/it_projects_concern.rb
+++ app/controllers/concerns/it_projects_concern.rb
@@ -1,7 +1,7 @@
 module Concerns
   module ItProjectsConcern
-    def main_page(it_project_name)
-      page_path it_project_name.split('.')[0].gsub('-', '_')
+    def main_page_of(it_project_name)
+      page_page_path it_project_name.split('.')[0].gsub('-', '_')
     end
   end
 end
