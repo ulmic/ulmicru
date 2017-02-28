4bc35660727aebd849fa8023c2b54416450eafa6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-02-28 17:12:02 +0400

    #FIX www

--- app/controllers/concerns/it_projects_concern.rb
+++ app/controllers/concerns/it_projects_concern.rb
@@ -1,7 +1,7 @@
 module Concerns
   module ItProjectsConcern
     def main_page_of(it_project_name)
-      page_page_path it_project_name.split('.')[0].gsub('-', '_')
+      page_page_path it_project_name.gsub('www.', '').split('.')[0].gsub('-', '_')
     end
   end
 end
