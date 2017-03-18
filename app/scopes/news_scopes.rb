cc9b39021622775dc9e167b958bed8dc477623a2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-03-18 13:48:24 +0400

    #FIX popular news

--- app/scopes/news_scopes.rb
+++ app/scopes/news_scopes.rb
@@ -15,10 +15,15 @@ module NewsScopes
       where(state: :confirmed).
       order('published_at DESC')
     }
-    scope :popular, -> {
-      where('published_at <= ? AND published_at >= ?', DateTime.now, DateTime.now - 1.month).
-      order('views DESC')
-    }
+    scope :popular, -> do
+      news = published.where('published_at <= ? AND published_at >= ?', DateTime.now, DateTime.now - 1.month)
+      views_count = news.reduce({}) do |hash, n|
+        hash.merge! n.id => n.page_views.count
+      end.sort_by { |_key, value| value }.reverse
+      views_count.map do |v|
+        News.find(v[0])
+      end
+    end
     scope :actual, -> { where("published_at > CURRENT_DATE - INTERVAL'6 days'") }
     scope :presented, -> { where.not(state: :removed) }
     scope :need_to_review, -> { where 'state = \'unviewed\' OR state = \'updated\'' }
