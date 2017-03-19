8bdf83e4ddba310b8121edcd1acc6eafb9ed56bc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-03-19 21:31:26 +0400

    #611 fix

--- app/helpers/web/admin/protocols_helper.rb
+++ app/helpers/web/admin/protocols_helper.rb
@@ -1,6 +1,6 @@
 module Web::Admin::ProtocolsHelper
   def protocol_color(protocol)
-    if protocol.document_id.present?
+    if protocol.document_id.present? && protocol.scan.present?
       :success
     else
       :danger
