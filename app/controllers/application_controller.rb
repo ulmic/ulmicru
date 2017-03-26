12f989837a8cb454342516e8e501b131dcba9871
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-03-26 23:21:40 +0400

    #FIX updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
       if request.host.include?('it-way.pro') && request.path != main_page_of('it-way.pro')
         redirect_to main_page_of 'it-way.pro'
       elsif request.host.include?('ul-lider.ru') && request.path != activity_lines_lider_ya_lider_path(15)
-        redirect_to activity_lines_lider_ya_lider_path(15)
+        redirect_to activity_lines_lider_ya_lider_path(15), status: :moved_permanently
       end
     end
   end
