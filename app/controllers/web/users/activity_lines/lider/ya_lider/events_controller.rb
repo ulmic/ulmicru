518f07b352136f644d7de2c900a87130d04bfe78
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-03-30 00:58:16 +0400

    #635 many updates

--- app/controllers/web/users/activity_lines/lider/ya_lider/events_controller.rb
+++ app/controllers/web/users/activity_lines/lider/ya_lider/events_controller.rb
@@ -1,6 +1,12 @@
 class Web::Users::ActivityLines::Lider::YaLider::EventsController < Web::Users::ActivityLines::Lider::YaLider::ApplicationController
   def new
-    @event_form = ::ActivityLines::Lider::YaLider::EventForm.new_with_model
-    @event_form.build_values!
+    case params[:step]
+    when '1'
+      choose_users
+      @event_form = ::EventForm.new_with_model
+    when '2'
+      @event_form = ::ActivityLines::Lider::YaLider::EventForm.new_with_model
+      @event_form.build_values!
+    end
   end
 end
--- app/models/activity_lines/lider/ya_lider/event.rb
+++ app/models/activity_lines/lider/ya_lider/event.rb
@@ -1,5 +1,6 @@
 class ActivityLines::Lider::YaLider::Event < ActiveRecord::Base
-  has_many :event_fields, class_name: 'ActivityLines::Lider::YaLider::EventField'
+  has_many :fields, class_name: 'ActivityLines::Lider::YaLider::EventField'
+
   belongs_to :participant, class_name: 'ActivityLines::Lider::YaLider::Participant'
   belongs_to :event
 end
