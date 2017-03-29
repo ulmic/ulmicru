96733ea9e049287daf666a6028b44b03484d4bc6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-03-29 23:29:18 +0400

    #635 many updates

--- app/forms/activity_lines/lider/ya_lider/event_form.rb
+++ app/forms/activity_lines/lider/ya_lider/event_form.rb
@@ -9,6 +9,15 @@ class ActivityLines::Lider::YaLider::EventForm < ApplicationReform
     validates :event_id, presence: true
   end
 
+  property :event, populate_if_empty: Event do
+    properties :title, :description, :main_photo, :place_ids, :creator_id, :begin_date, :end_date, :activity_line_id, 
+               :organizer_type, :organizer_id, required: true
+
+    collection :registrations do
+      properties :user_id, :event_id, :role
+    end
+  end
+
   def build_values!
     titles = ::ActivityLines::Lider::YaLider::ParticipantField.title.values
     titles.each do |title|
