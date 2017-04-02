ed1eec097f265236442489998c3c8ab6f74b1e80
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-04-03 01:09:59 +0400

    #643 fix delivery

--- app/controllers/web/admin/delivery/campaign/test_sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class Web::Admin::Delivery::Campaign::TestSessionsController < Web::Admin::Delivery::ApplicationController
-  def create
-    campaign = ::Delivery::Campaign.find(params[:id]).decorate
-    contacts = campaign.receivers.includes(user: :subscriptions).where(state: :ready).to_a
-    ::Admin::TestReceiversJob.perform_later contacts, campaign.title, campaign.body, campaign.link, campaign.image.url, campaign.id
-    redirect_to admin_delivery_campaign_path(campaign.id)
-  end
-end
---
+++ db/migrate/20170402205815_remove_table.rb
@@ -0,0 +1,4 @@
+class RemoveTable < ActiveRecord::Migration
+  def change
+  end
+end
