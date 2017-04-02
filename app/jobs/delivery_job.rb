fbbe693bc6d77b374d26e31f5cfd796c49f96375
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-04-03 01:34:22 +0400

    #643 fix

--- app/jobs/delivery_job.rb
+++ app/jobs/delivery_job.rb
@@ -4,7 +4,7 @@ class DeliveryJob < ActiveJob::Base
   def perform(contacts, title, text, link, image, tokens)
     contacts.each_slice(100) do |combination|
       combination.each do |contact|
-        UserMailer.delay.send :just_message, contact.user.decorate, contact.user.email, title, text, link, image, tokens[contact.user_id]
+        UserMailer.delay.send :just_message, contact.user.decorate, contact.user.email, title, text, link, image, tokens[contact.user_id.to_s]
         contact.send_email
       end
     end
