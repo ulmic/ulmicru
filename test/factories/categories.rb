f980d6f643f6192b8a33d22f8b71dd2046b2fc9e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-04-08 22:59:32 +0400

    #TEST fix

--- test/factories/categories.rb
+++ test/factories/categories.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name { generate :string }
     parent_id { Category.last ? Category.last.id : nil }
     state { Category.state_machines[:state].states.map(&:name).first.to_s }
+    id { Category.maximum(:id) + 1 }
 
     trait :contacts do
       name 'Контакты'
