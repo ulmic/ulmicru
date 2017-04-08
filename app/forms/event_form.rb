affba47e0be277ffd1264574c2f5d20856e29165
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-04-08 13:03:59 +0400

    #FIX creating events

--- app/forms/event_form.rb
+++ app/forms/event_form.rb
@@ -1,8 +1,9 @@
 class EventForm < ApplicationForm
   self.main_model = :event
 
-  attributes :title, :description, :main_photo, :place_ids, :creator_id, :begin_date, :end_date, :activity_line_id, 
+  attributes :title, :description, :main_photo, :creator_id, :begin_date, :end_date, :activity_line_id, 
              :organizer_type, :organizer_id, required: true
+  attributes :place_ids
 
   association :registrations do
     attributes :user_id, :event_id, :role
