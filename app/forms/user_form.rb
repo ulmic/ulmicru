da1297717394de9221875cc6fb050e52cd3a9de1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-04-25 02:13:45 +0400

    #FIX user form

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -1,3 +1,3 @@
 class UserForm < ApplicationReform
-  properties :role, :email, :first_name, :last_name, :patronymic, :password, :team_ids, :state_event, :questionary_state, :email_state
+  properties :role, :email, :first_name, :last_name, :patronymic, :password, :team_ids, :state_event, :questionary_state, :email_state, :email_state_event
 end
