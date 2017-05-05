1d14fe0eff420a0a6864586c555590525a1732b6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-05-05 18:05:29 +0400

    #TEST updates

--- db/schema.rb
+++ db/schema.rb
@@ -122,9 +122,9 @@ ActiveRecord::Schema.define(version: 20170505131901) do
   create_table "activity_lines_lider_ya_lider_participations", force: :cascade do |t|
     t.integer  "participant_id"
     t.integer  "stage_id"
-    t.text     "state"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.text     "state"
   end
 
   create_table "activity_lines_lider_ya_lider_stages", force: :cascade do |t|
@@ -265,17 +265,17 @@ ActiveRecord::Schema.define(version: 20170505131901) do
     t.text     "email"
     t.text     "first_name"
     t.text     "last_name"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.text     "state",      default: "active"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.text     "state",      default: "subscribed"
   end
 
   create_table "delivery_receivers", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "campaign_id"
-    t.text     "state",       default: "ready"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.text     "state"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "documents", force: :cascade do |t|
@@ -567,9 +567,6 @@ ActiveRecord::Schema.define(version: 20170505131901) do
     t.integer "team_id"
     t.integer "user_id"
   end
-
-  add_index "teams_users", ["team_id"], name: "index_teams_users_on_team_id", using: :btree
-  add_index "teams_users", ["user_id"], name: "index_teams_users_on_user_id", using: :btree
 
   create_table "test_paper_questions", force: :cascade do |t|
     t.text     "text"
