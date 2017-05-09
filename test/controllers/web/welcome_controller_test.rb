a96eb9ba5af9e9ea9fffc62f6f0452f6e001b86a
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-05-09 21:52:14 +0400

    #160 update tests

--- test/controllers/web/welcome_controller_test.rb
+++ test/controllers/web/welcome_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class Web::WelcomeControllerTest < ActionController::TestCase
+  setup do
+    @time_quantum = 2.seconds
+  end
+
   test 'should get index' do
     10.times do
       get :index
@@ -15,40 +19,57 @@ class Web::WelcomeControllerTest < ActionController::TestCase
   end
 
   test 'should get index 100 times' do
-    #100.times do |i|
+    100.times do |i|
       time = Time.now
       get :index
       duration = Time.now - time
-      assert duration < 1.second, duration
+      assert duration < @time_quantum, duration
       assert_response :success, @response.body
-    #  print "#{i} of 100\r"
-    #end
+      print "#{i} of 100\r"
+    end
   end
 
   test 'should get index with authorized user' do
-    user = create :user
-    sign_in user
-    100.times do
-      get :index
-      assert_response :success, @response.body
+    if ENV['DB'] == 'prod'
+      user = create :user
+      sign_in user
+      100.times do |i|
+        time = Time.now
+        get :index
+        duration = Time.now - time
+        assert duration < @time_quantum, duration
+        assert_response :success, @response.body
+        print "#{i} of 100\r"
+      end
     end
   end
 
   test 'should get index with authorize member' do
-    member = create :member
-    sign_in member
-    100.times do
-      get :index
-      assert_response :success, @response.body
+    if ENV['DB'] == 'prod'
+      member = create :member
+      sign_in member
+      100.times do |i|
+        time = Time.now
+        get :index
+        duration = Time.now - time
+        assert duration < @time_quantum, duration
+        assert_response :success, @response.body
+        print "#{i} of 100\r"
+      end
     end
   end
 
   test 'should get index with authorize admin' do
-    admin = create :admin
-    sign_in admin
-    100.times do
-      get :index
-      assert_response :success, @response.body
+    if ENV['DB'] == 'prod'
+      admin = create :admin
+      sign_in admin
+      100.times do
+        time = Time.now
+        get :index
+        duration = Time.now - time
+        assert duration < @time_quantum, duration
+        assert_response :success, @response.body
+      end
     end
   end
 end
