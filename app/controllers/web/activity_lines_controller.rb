db6d204e269327f58ff3f2cadc8895a73a135a9c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-05-23 02:50:01 +0400

    #685 add news to activity_lines page

--- app/controllers/web/activity_lines_controller.rb
+++ app/controllers/web/activity_lines_controller.rb
@@ -2,5 +2,6 @@ class Web::ActivityLinesController < Web::ApplicationController
   def show
     @activity_line = ActivityLine.find(params[:id]).decorate
     @future_events = @activity_line.events.confirmed.future.decorate
+    @last_news = NewsDecorator.decorate_collection News.order(published_at: :asc).published.where(id: @activity_line.tags.news.map(&:record_id)).last(8).reverse
   end
 end
