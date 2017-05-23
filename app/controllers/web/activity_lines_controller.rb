2b23f361ff74fdf137eafbe85a9890ab2fc4c65f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-05-23 04:00:07 +0400

    #685 view for activity_lines

--- app/controllers/web/activity_lines_controller.rb
+++ app/controllers/web/activity_lines_controller.rb
@@ -5,6 +5,7 @@ class Web::ActivityLinesController < Web::ApplicationController
     @future_events = @activity_line.events.confirmed.future.decorate
     @last_news = NewsDecorator.decorate_collection News.order(published_at: :asc).published.where(id: @activity_line.tags.news.map(&:record_id)).last(8).reverse
     @old_leaders = Position.old_positions.where(title: @activity_line.leader_title).map &:member
+    View.create! user_id: current_user&.id, record_id: @activity_line.id, record_type: 'ActivityLine'
   end
 
   private
