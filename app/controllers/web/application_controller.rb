c5a87eef8fc13df6aec18a1e07e7fa9939a2bdc6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-07-30 22:41:53 +0400

    #FIX some fixes

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -40,7 +40,10 @@ class Web::ApplicationController < ApplicationController
     @activity_lines = ActivityLine.official.decorate
     @feedback = FeedbackForm.new_with_model
     @rss_article_id = 19
-    @who_are_we = Article.find 11
+    @who_we_are = Article.find 11
+    @open_departament = Article.find 76
+    @requisites = Article.find 15
+    @contacts = Article.find 16
   end
 
   def load_members_menu
