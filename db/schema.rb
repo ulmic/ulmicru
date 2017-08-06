508d3c0380443bc3cc00b940e6f0e5fe89ba41fe
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-08-06 23:59:58 +0400

    #717 add version to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613070246) do
+ActiveRecord::Schema.define(version: 20170806194517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -508,6 +508,7 @@ ActiveRecord::Schema.define(version: 20170613070246) do
     t.text     "state"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "version"
   end
 
   create_table "protocol_absents", force: :cascade do |t|
