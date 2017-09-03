a36857629ecfb7ea2425dd0f7a54bb72e14172cf
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-09-04 03:23:51 +0400

    #FIX teams work

--- app/models/team/committee.rb
+++ app/models/team/committee.rb
@@ -2,5 +2,5 @@ class Team::Committee < Team
   belongs_to :project, polymorphic: true
 
   extend Enumerize
-  enumerize :project_type, in: [ 'ActivityLines::Lider::YaLider' ]
+  enumerize :project_type, in: [ 'ActivityLines::Lider::YaLider', 'Project' ]
 end
